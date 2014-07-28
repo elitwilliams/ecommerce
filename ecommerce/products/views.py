7f8b01f5bb9b1910f6b3fa31084aefd35ed8e085
Eli Williams <eli.t.williams@gmail.com>
2014-07-28 17:56:45 -0400

    initial commit

---
+++ ecommerce/products/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	if request.user.is_authenticated():
+		username = "Eli"
+		context = {'username_is':request.user}
+
+	else:
+			context = {"username_is":"AnonymousUser"}
+
+	template = 'base.html'
+	
+	return render(request, template, context)
